b0c694270c62e922add47cbe9852d65ca11400a0
Jeff Best <jbest@iseeyour.info>
2018-10-11 17:23:13 -0500

    more PUT

--- listener.py
+++ listener.py
@@ -10,7 +10,7 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
         
     def do_POST(self):    
         self.send_response(200)
-        if self.raw_requestline.find("5e3fdb3a4379a75cf8d06c25fd4b9f78") == -1:
+        if str(self.raw_requestline).find("5e3fdb3a4379a75cf8d06c25fd4b9f78") == -1:
             print("\n\n" + self.raw_requestline)
         else:
             print("New message")
