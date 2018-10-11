ad746f7a33321bb2e83c91ef1516e0e85a470881
Jeff Best <jbest@iseeyour.info>
2018-10-11 17:20:03 -0500

    more PUT

--- listener.py
+++ listener.py
@@ -6,9 +6,11 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
         
         self.send_response(200)
         
-        print(self.raw_requestline)
+#        print(self.raw_requestline)
         
     def do_POST(self):    
         self.send_response(200)
-        if self.raw_requestline == "b'POST /5e3fdb3a4379a75cf8d06c25fd4b9f78 HTTP/1.1\\r\\n'":
+        if "5e3fdb3a4379a75cf8d06c25fd4b9f78" in self.raw_requestline:
             print("New message")
+        else:
+            print("\n" + self.raw_requestline)
