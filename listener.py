2a47583b877226cd54da2ae40ea9f10a3b0bc88f
Jeff Best <jbest@iseeyour.info>
2018-10-12 08:59:23 -0500

    corrected HELP

--- listener.py
+++ listener.py
@@ -14,10 +14,9 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
             print("\n\n" + self.raw_requestline)
         else:
             gm_api_key = "rYeaadubaCmn1TgT3zcXAgJz49eSpn4SRp65x4Wg"
-            scoreboard_bot_id = "799b29a881dc3652b0866e13c1"
+            scoreboard_bot_id = "49965cd413951e7f2214fa9dc1"
             client = groupy.Client.from_token(gm_api_key)
-            dp_group_id = "45030392"
-            #this is the real deadpool'd id: 4476689
+            dp_group_id = "4476689"
            
             for i in client.groups.list_all():
                 if i.id == dp_group_id:
