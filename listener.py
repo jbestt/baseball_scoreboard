2953e73ad1602e8b1b0310601593bf3c8399d337
Jeff Best <jbest@iseeyour.info>
2018-10-11 21:17:05 -0500

    more PUT

--- listener.py
+++ listener.py
@@ -1,4 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import groupy
 
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
     
@@ -14,4 +15,18 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
             print("\n\n" + self.raw_requestline)
         else:
             print("New message")
+           
+            gm_api_key = "rYeaadubaCmn1TgT3zcXAgJz49eSpn4SRp65x4Wg"
+            scoreboard_bot_id = "799b29a881dc3652b0866e13c1"
+            client = groupy.Client.from_token(gm_api_key)
+            dp_group_id = "45030392"
+            #this is the real deadpool'd id: 4476689
+           
+            for i in client.groups.list_all():
+                if i.id == dp_group_id:
+                    deadpool = i
+       
+            message_page = deadpool.messages.list()
+            
+            print(deadpool.messages.list_all().text[0])
             
