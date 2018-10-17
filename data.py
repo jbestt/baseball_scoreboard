47446111a4fff3e76e857cd2f570d0134646f31a
Jeff Best <jbest@iseeyour.info>
2018-10-16 22:16:51 -0500

    added runners

--- data.py
+++ data.py
@@ -42,7 +42,8 @@ def get_tv_runner_info(pk,year=now.year,month=now.month,day=now.day):
     request = http.request('GET','http://gd.mlb.com/components/game/mlb/year_{0}/month_{1:02d}/day_{2:02d}/master_scoreboard.json'.format(year,month,day))
     alt_scoreboard = json.loads(request.data)
     alt_info = ""
-    if alt_scoreboard['data']['games']['game']['game_media']:
+    if alt_scoreboard['data']['games']['game'][0] == 'game_media':
+        print("one game today")
         alt_info = alt_scoreboard['data']['games']['game']
 #    elif '0' in alt_scoreboard['data']['games']['game']:
     else:
